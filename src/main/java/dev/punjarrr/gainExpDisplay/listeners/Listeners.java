b4ffb9449076e868e9d542470b87f9f953ff1c8f
Punnathorn Vettayanont <punnathorn.vet@gmail.com>
2025-12-12 01:47:34 +0700

    - Added 1.17-1.18.x support - Added new config sections for gaining XP and mending repair toggle

--- src/main/java/dev/punjarrr/gainExpDisplay/listeners/Listeners.java
+++ src/main/java/dev/punjarrr/gainExpDisplay/listeners/Listeners.java
@@ -1,7 +1,6 @@
 package dev.punjarrr.gainExpDisplay.listeners;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.MiniMessage;
+import dev.punjarrr.gainExpDisplay.utils.TextUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,7 +13,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Listeners implements Listener {
 
     private final JavaPlugin plugin;
-    private final MiniMessage mm = MiniMessage.miniMessage();
 
     public Listeners(JavaPlugin plugin) {
         this.plugin = plugin;
@@ -22,21 +20,21 @@ public class Listeners implements Listener {
 
     @EventHandler
     public void onExpChange(PlayerExpChangeEvent e) {
+        if (!plugin.getConfig().getBoolean("gain-xp.enabled", true)) return;
+
         Player player = e.getPlayer();
         int amount = e.getAmount();
 
-        String format = plugin.getConfig().getString("messages.gain-xp", "");
+        String format = plugin.getConfig().getString("gain-xp.message", "");
         if (format.isEmpty()) return;
 
-        Component message = mm.deserialize(
-                format.replace("%amount%", String.valueOf(amount))
-        );
-
-        player.sendActionBar(message);
+        player.sendActionBar(TextUtil.parse(format, amount));
     }
 
     @EventHandler
     public void onPlayerItemMend(PlayerItemMendEvent e) {
+        if (!plugin.getConfig().getBoolean("mending-repair.enabled", true)) return;
+
         Player player = e.getPlayer();
         int amount = e.getRepairAmount();
         ItemStack item = e.getItem();
@@ -46,16 +44,9 @@ public class Listeners implements Listener {
         int maxDurability = item.getType().getMaxDurability();
         int durability = maxDurability - damageable.getDamage() + amount;
 
-        String format = plugin.getConfig().getString("messages.mending-repair", "");
+        String format = plugin.getConfig().getString("mending-repair.message", "");
         if (format.isEmpty()) return;
 
-        Component message = mm.deserialize(
-                format
-                        .replace("%amount%", String.valueOf(amount))
-                        .replace("%durability%", String.valueOf(durability))
-                        .replace("%max%", String.valueOf(maxDurability))
-        );
-
-        player.sendActionBar(message);
+        player.sendActionBar(TextUtil.parse(format, amount, durability, maxDurability));
     }
 }
---
+++ src/main/java/dev/punjarrr/gainExpDisplay/utils/TextUtil.java
@@ -0,0 +1,26 @@
+package dev.punjarrr.gainExpDisplay.utils;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+
+public class TextUtil {
+
+    private static final MiniMessage mm = MiniMessage.miniMessage();
+
+    public static Component parse(String input, int amount) {
+
+        return mm.deserialize(
+                input.replace("%amount%", String.valueOf(amount))
+        );
+    }
+
+    public static Component parse(String input, int amount, int durability, int maxDurability) {
+
+        return mm.deserialize(
+                input
+                        .replace("%amount%", String.valueOf(amount))
+                        .replace("%durability%", String.valueOf(durability))
+                        .replace("%max%", String.valueOf(maxDurability))
+        );
+    }
+}
